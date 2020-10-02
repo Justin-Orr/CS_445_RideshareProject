b3a0410ed7ac75818feca53820cdb9023c806532
Justin-Orr <jj.oreo98@gmail.com>
2020-10-02 14:45:11 -0500

    Delete Vehicle and account type attribute from Account class

--- src/com/company/myapp/Account.java
+++ src/com/company/myapp/Account.java
@@ -7,8 +7,7 @@ public class Account {
 	private int aid, numberOfRatings;
 	private double averageRating;
 	private boolean isActive;
-	private String accountType, firstName, lastName, phoneNumber, picture, dateCreated;
-	private Vehicle vehicle;
+	private String firstName, lastName, phoneNumber, picture, dateCreated;
 	
 	public Account() {
 		//Default constructor
@@ -19,12 +18,10 @@ public class Account {
 		this.numberOfRatings = 0;
 		this.averageRating = 0;
 		this.isActive = false;
-		this.accountType = "unset";
 		this.firstName = first_name; 
 		this.lastName = last_name;
 		this.phoneNumber = phone_number;
 		this.picture = picture;
-		this.vehicle = new Vehicle();
 		this.dateCreated = TimeStamp.stamp();
 	}
 	
@@ -34,13 +31,11 @@ public class Account {
 		a.setNumberOfRatings(json.getInt("numberOfRatings"));
 		a.setAverageRating(json.getDouble("averageRating"));
 		a.setActive(json.getBoolean("active"));
-		a.setAccountType(json.getString("accountType"));
 		a.setFirstName(json.getString("firstName"));
 		a.setLastName(json.getString("lastName"));
 		a.setPhoneNumber(json.getString("phoneNumber"));
 		a.setPicture(json.getString("picture"));
 		a.setDateCreated(json.getString("dateCreated"));
-		a.setVehicle(Vehicle.jsonToVehicle(json.getJSONObject("vehicle")));
 		return a;
 	}
 	
@@ -52,27 +47,6 @@ public class Account {
 		obj.put("is_active", isActive);
 		
 		return obj;
-	}
-	
-	public int activateAccount(String type) {
-		if (type.compareTo("driver") == 0)
-			return activateDriver();
-		else if (type.compareTo("rider") == 0)
-			return activateRider();
-		else
-			return -1;
-	}
-	
-	public int activateDriver() {
-		//Place method call here to ping user to grab vehicle data
-		isActive = true;
-		vehicle = new Vehicle();
-		return 0;
-	}
-	
-	public int activateRider() {
-		isActive = true;
-		return 0;
 	}
 	
 	public int getAid() {
@@ -105,14 +79,6 @@ public class Account {
 
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
-	}
-
-	public String getAccountType() {
-		return accountType;
-	}
-
-	public void setAccountType(String accountType) {
-		this.accountType = accountType;
 	}
 
 	public String getFirstName() {
@@ -153,14 +119,6 @@ public class Account {
 
 	public void setDateCreated(String dateCreated) {
 		this.dateCreated = dateCreated;
-	}
-
-	public Vehicle getVehicle() {
-		return vehicle;
-	}
-
-	public void setVehicle(Vehicle vehicle) {
-		this.vehicle = vehicle;
 	}
 
 }
